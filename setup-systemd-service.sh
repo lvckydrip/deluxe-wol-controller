794cffc177f0a0572efade40c64622f4c75370a9
Florian <florianfietze@gmail.com>
2024-12-15 21:26:26 +0100

    added an setup file

---
+++ setup-systemd-service.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+script_dir=$(dirname "$(realpath "$0")")
+swapfile_content="CONF_SWAPSIZE=4096"
+
+if [ "$(cat "/etc/dphys-swapfile")" != "$swapfile_content" ]; then
+  echo "Increasing swap file size..."
+  sudo dphys-swapfile swapoff
+  sudo echo $swapfile_content | sudo tee "/etc/dphys-swapfile"
+  sudo dphys-swapfile setup
+  sudo dphys-swapfile swapon
+  echo "Successfully changed swap file size. Rebooting now..."
+  sudo reboot
+fi
+
+echo "Started installing npm and pnpm..."
+sudo apt install npm -y
+sudo apt install pnpm -y
+echo "Successfully installed npm and pnpm."
+
+echo "Run pnpm install..."
+cd "$script_dir" || exit
+sudo pnpm install
+echo "Successfully finished pnpm install."
+
+echo "Creating systemd service..."
+cat <<EOL | sudo tee "/etc/systemd/system/deluxe-wol-controller.service"
+[Unit]
+Description=Deluxe WakeOnLan Controller Service
+After=multi-user.target
+
+[Service]
+ExecStart=/usr/bin/sudo /usr/bin/npm run start
+Type=simple
+Restart=always
+WorkingDirectory=$script_dir
+
+[Install]
+WantedBy=multi-user.target
+EOL
+echo "Successfully created systemd service at /etc/systemd/system/deluxe-wol-controller.service"
+
+echo "Enabling service..."
+sudo systemctl daemon-reload
+sudo systemctl enable deluxe-wol-controller.service
+sudo systemctl start deluxe-wol-controller.service
+
+echo "Service status:"
+sudo systemctl status deluxe-wol-controller.service
