eed4e70d83794f60f858e3279c459d25db08e6f3
Florian <florianfietze@gmail.com>
2024-12-16 20:24:40 +0100

    added responsive grid to support mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,15 @@ export default async function Home() {
   const data = await computerActions();
 
   return (
-      <main className="grid grid-cols-4">
+      <main className="grid lg:grid-cols-4">
         <div className="bg-zinc-800 rounded-2xl flex items-center justify-center m-4">
           <Link href="/create" className="w-max text-nowrap text-center rounded-full transition-colors bg-zinc-50 text-zinc-950 hover:bg-zinc-400 h-10 p-2 m-4">
             Create new Computer
           </Link>
         </div>
+        {data?.map((item) => <ComputerItem key={item.id} item={item} />)}
+        {data?.map((item) => <ComputerItem key={item.id} item={item} />)}
+        {data?.map((item) => <ComputerItem key={item.id} item={item} />)}
         {data?.map((item) => <ComputerItem key={item.id} item={item} />)}
       </main>)
 }
